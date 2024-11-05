565c76f4f95caa2dab72c9255af010be7e071d3d
LimphoMokole <600370@student.belgiumcampus.ac.za>
2024-11-05 19:09:00 +0200

    Imported moduels into the FileHandler class.

--- Student Management System using Windows Forms_LLG/Data_Layer/FileHandler.cs
+++ Student Management System using Windows Forms_LLG/Data_Layer/FileHandler.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Student_Management_System_using_Windows_Forms_LLG.Presentation_Layer;
+using Student_Management_System_using_Windows_Forms_LLG.Business_Logic_Layer;
+using System.IO;
 
 namespace Student_Management_System_using_Windows_Forms_LLG.Data_Layer
 {
-    internal class FileHandler
+    class FileHandler
     {
     }
 }
