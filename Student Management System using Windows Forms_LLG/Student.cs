151fe1263268e3b786368b5ff5704c304242eec9
Gift <direodirile95@gmail.com>
2024-11-05 21:33:59 +0200

    created a class student.cs within the class i addede suitable properties of a the student class as well .After doing that i added to methods one that is going to format the file when the data is written to the textfile,same as well with the the data that is going to be retrieved when the data is requested by the user from the the userinterface

--- Student Management System using Windows Forms_LLG/Student.cs
+++ Student Management System using Windows Forms_LLG/Student.cs
@@ -8,6 +8,33 @@ namespace Student_Management_System_using_Windows_Forms_LLG
 {
     public class Student
     {
-        public string
+        public string StudentID { get;set; }
+
+        public string Name { get;set; }
+        public int Age { get;set; }
+        public string Course { get;set; }
+        
+        public Student(string studentID, string name , int age ,string course)
+        {
+            this.StudentID = studentID;
+            this.Name = name;
+            this.Age = age;
+            this.Course = course;
+        
+        
+        
+        }
+
+        public string ToFileFormat()
+        {
+            return $"{StudentID},{Name},{Age},{Course}";
+        }
+
+        public static Student FromFileFormat(string line)
+        {
+            var parts = line.Split(',');
+
+            return new Student(parts[0], parts[1], int.Parse(parts[2]), parts[3]);
+        }
     }
 }
